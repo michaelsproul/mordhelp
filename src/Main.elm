9e2758c94998c2e07bc87ff587847cc501379b5e
Michael Sproul <michael@sigmaprime.io>
2023-12-26 22:34:03 +1100

    JSON decoding & warband upload

--- src/Main.elm
+++ src/Main.elm
@@ -2,11 +2,15 @@ module Main exposing (..)
 
 import Attack exposing (toHitByWs, toWoundByStrength)
 import Browser exposing (Document)
-import Html exposing (Html, div, h1, table, tbody, td, text, th, thead, tr)
+import File exposing (File)
+import File.Select as Select
+import Html exposing (Html, button, div, h1, span, table, tbody, td, text, th, thead, tr)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Json.Decode as Decode exposing (Decoder)
 import List exposing (map, range)
-import Warband
+import Task exposing (Task)
+import Warband exposing (Warband, decodeWarband)
 
 
 
@@ -23,16 +27,18 @@ main =
 
 type Page
     = GenericStats
+    | WarbandInfo
 
 
 type alias Model =
     { page : Page
+    , warband : Maybe Warband
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { page = GenericStats }, Cmd.none )
+    ( { page = WarbandInfo, warband = Nothing }, Cmd.none )
 
 
 
@@ -40,12 +46,55 @@ init _ =
 
 
 type Msg
-    = Msg
+    = WarbandRequested
+    | WarbandSelected File
+    | WarbandLoaded Warband
+    | ChangePage Page
+    | Noop
+
+
+resultToTask : Result x a -> Task x a
+resultToTask res =
+    case res of
+        Ok v ->
+            Task.succeed v
+
+        Err e ->
+            Task.fail e
+
+
+runDecoder : Decoder a -> String -> Task Decode.Error a
+runDecoder decoder value =
+    Decode.decodeString decoder value |> resultToTask
+
+
+handleError : (a -> Msg) -> Result x a -> Msg
+handleError f res =
+    case res of
+        Ok v ->
+            f v
+
+        Err e ->
+            Debug.log ("failed: " ++ Debug.toString e) Noop
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        WarbandRequested ->
+            ( model, Select.file [ "application/json" ] WarbandSelected )
+
+        WarbandSelected file ->
+            ( model, Task.attempt (handleError WarbandLoaded) (File.toString file |> Task.andThen (runDecoder decodeWarband)) )
+
+        WarbandLoaded warband ->
+            ( { page = model.page, warband = Just warband }, Cmd.none )
+
+        ChangePage page ->
+            ( { page = page, warband = model.warband }, Cmd.none )
+
+        Noop ->
+            ( model, Cmd.none )
 
 
 
@@ -56,7 +105,10 @@ view : Model -> Document Msg
 view model =
     case model.page of
         GenericStats ->
-            { title = "Mordhelp: Rules", body = [ viewGenericStats ] }
+            { title = "Rules - Mordhelp", body = [ viewGenericStats ] }
+
+        WarbandInfo ->
+            { title = "Warband - Mordhelp", body = [ viewWarband model.warband ] }
 
 
 table2dRow : Int -> Int -> (Int -> Int -> String) -> List (Html Msg)
@@ -85,10 +137,19 @@ table2d width height f =
         ]
 
 
+siteNav : Html Msg
+siteNav =
+    div []
+        [ span [ onClick (ChangePage GenericStats) ] [ text "Stats" ]
+        , span [ onClick (ChangePage WarbandInfo) ] [ text "Warband" ]
+        ]
+
+
 viewGenericStats : Html Msg
 viewGenericStats =
     div []
         [ h1 [] [ text "Mordhelp" ]
+        , siteNav
         , table2d 10 10 (\attackerWs defenderWs -> String.fromInt (toHitByWs attackerWs defenderWs))
         , table2d 10
             10
@@ -97,6 +158,15 @@ viewGenericStats =
                     |> Maybe.map String.fromInt
                     |> Maybe.withDefault "-"
             )
+        ]
+
+
+viewWarband : Maybe Warband -> Html Msg
+viewWarband maybeWarband =
+    div []
+        [ h1 [] [ text "Mordhelp" ]
+        , siteNav
+        , button [ onClick WarbandRequested ] [ text "Upload Warband" ]
         ]
 
 
--- src/Warband.elm
+++ src/Warband.elm
@@ -1,4 +1,7 @@
-module Warband exposing (Equipment, Profile, Unit, UnitKind, WeaponKind, WeaponStrength)
+module Warband exposing (Equipment, Profile, Unit, UnitKind, Warband, WeaponKind, WeaponStrength, decodeWarband)
+
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (hardcoded, optional, required)
 
 
 minCharacteristic : Int
@@ -44,6 +47,7 @@ type WeaponStrength
 type Equipment
     = Weapon
         { name : String
+        , kind : WeaponKind
         , strength : WeaponStrength
         }
 
@@ -53,3 +57,98 @@ type alias Unit =
     , profile : Profile
     , equipment : List Equipment
     }
+
+
+type alias Warband =
+    { units : List Unit }
+
+
+decodeLiteral : String -> String -> Decoder String
+decodeLiteral desired actual =
+    if actual == desired then
+        Decode.succeed desired
+
+    else
+        Decode.fail <| "expected `" ++ desired ++ "` got `" ++ actual
+
+
+decodeLiteralAsValue : String -> a -> String -> Decoder a
+decodeLiteralAsValue desired value actual =
+    decodeLiteral desired actual |> Decode.map (\_ -> value)
+
+
+decodeUnitKind : Decoder UnitKind
+decodeUnitKind =
+    Decode.oneOf
+        [ string |> Decode.andThen (decodeLiteralAsValue "hero" Hero)
+        , string |> Decode.andThen (decodeLiteralAsValue "henchman" Henchman)
+        ]
+
+
+decodeWeaponKind : Decoder WeaponKind
+decodeWeaponKind =
+    Decode.oneOf
+        [ string |> Decode.andThen (decodeLiteralAsValue "melee" Melee)
+        , string |> Decode.andThen (decodeLiteralAsValue "ballistic" Ballistic)
+        ]
+
+
+decodeProfile : Decoder Profile
+decodeProfile =
+    Decode.succeed Profile
+        |> required "name" string
+        |> required "kind" decodeUnitKind
+        |> required "movement" int
+        |> required "weaponSkill" int
+        |> required "ballisticSkill" int
+        |> required "strength" int
+        |> required "toughness" int
+        |> required "wounds" int
+        |> required "initiative" int
+        |> required "attacks" int
+        |> required "leadership" int
+
+
+decodeWeaponStrength : Decoder WeaponStrength
+decodeWeaponStrength =
+    string
+        |> Decode.andThen
+            (\s ->
+                if String.startsWith "+" s then
+                    case String.toInt (String.dropLeft 1 s) of
+                        Just n ->
+                            Decode.succeed (StrengthMod n)
+
+                        Nothing ->
+                            Decode.fail <| "invalid strength mod: " ++ s
+
+                else
+                    case String.toInt s of
+                        Just n ->
+                            Decode.succeed (StrengthConst n)
+
+                        Nothing ->
+                            Decode.fail <| "invalid strength const: " ++ s
+            )
+
+
+decodeEquipment : Decoder Equipment
+decodeEquipment =
+    Decode.succeed (\name kind strength -> Weapon { name = name, kind = kind, strength = strength })
+        |> required "name" string
+        |> required "kind" decodeWeaponKind
+        |> required "strength" decodeWeaponStrength
+
+
+decodeUnit : Decoder Unit
+decodeUnit =
+    Decode.succeed Unit
+        |> required "count" int
+        |> required "profile" decodeProfile
+        |> required "equipment" (list decodeEquipment)
+
+
+decodeWarband : Decoder Warband
+decodeWarband =
+    Decode.succeed Warband
+        |> required "units" (list decodeUnit)
